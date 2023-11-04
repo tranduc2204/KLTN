440292aecc6dc0b91e3faddea2e734d916d1580f
tranduc2204 <trandc3015@gmail.com>
2023-11-04 13:03:58 +0700

    update 4 11

--- src/form/FormCheckIn.java
+++ src/form/FormCheckIn.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package form;
+package form; 
 
 import connect.Connect;
 import java.sql.Connection;
@@ -49,7 +49,7 @@ public class FormCheckIn extends javax.swing.JPanel {
        
 
         inittable();
-        loaddulieu1();
+//        loaddulieu1();
     }
     
     private void inittable() {
